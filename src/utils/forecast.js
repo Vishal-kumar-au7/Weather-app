60d109e30b7173accce0349cca4696b1992b3fee
vishal <vishik@gmail.com>
2020-07-08 23:36:04 +0530

    update humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -18,7 +18,7 @@ const forecast = (latitude,longitude, callback) => {
 
     else{
             callback(undefined,body.current.weather_descriptions[0]+", Current Temperature is: "+body.current.temperature
-                +" degrees out." +" It feels like "+ body.current.feelslike +"degrees out." +" The Humidity is "+body.current.humidity+"%.")
+                +" degrees out." +" It feels like "+ body.current.feelslike +" degrees out." +" The Humidity is "+body.current.humidity+"%.")
         }
  })
 
